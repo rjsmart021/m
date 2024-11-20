8f72f2e71ec81b01afd4e34a55d89226296cca4e
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-19 22:07:38 -0600

    Update Characterdetail.jsx

--- Characterdetail.jsx
+++ Characterdetail.jsx
@@ -1,1 +1,24 @@
+import "../styles/Characters.scss"
 
+export default function Characters({ data, onClick }) {
+  return (
+    <div className="characters">
+      {data.map(character => {
+        return (
+          <div
+            key={character.id}
+            className="characterCard"
+            style={{
+              background: `url(${character.thumbnail.path}.${character.thumbnail.extension}) no-repeat center`,
+              backgroundSize: "cover",
+            }}
+            onClick={() => onClick(character.id)}
+          >
+            <div className="caption">{character.name}</div>
+            <div className="caption bottom">View Comics</div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
