dbde84a5e04e41b7f20e57ca7b8d0556d9895c93
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-20 07:39:47 -0600

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,18 @@
+// About.js
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <h2>About Me</h2>
+      <div className="about-content">
+        <p>
+          Hello! My name is Reggie. I am a Software developer and Data Analyst. I am enjoying this course.  
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ contact.jsx
@@ -0,0 +1,6 @@
+class Car extends React.Component {
+    render() {
+      return <h2>Email:rjsmart021@gmail.Component, linkedin: https://www.linkedin.com/feed/</h2>;
+    }
+  }
+
